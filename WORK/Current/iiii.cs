e0730e834ad685ae4a6c90b9fb066a3bf1065347
BonickD <bountywinner@mail.ru>
2021-12-27 17:52:08 +0300

    abbb

---
+++ WORK/Current/iiii.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections.Generic;
+using Oxide.Core;
+using Oxide.Core.Plugins;
+using Oxide.Game.Rust.Cui;
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("iiii", "AhigaO#4485", "1.0.0")]
+    internal class iiii : RustPlugin
+    {
+        #region Static
+
+        private const string Layer = "UI_iiii";
+        private int ImageLibraryCheck = 0;
+        private Configuration _config;
+        private Dictionary<ulong, Data> data;
+        [PluginReference] private Plugin ImageLibrary;
+
+        #endregion
+
+        #region Config
+
+        private class Configuration
+        {
+
+        }
+
+        protected override void LoadConfig()
+        {
+            base.LoadConfig();
+            try
+            {
+                _config = Config.ReadObject<Configuration>();
+                if (_config == null) throw new Exception();
+                SaveConfig();
+            }
+            catch
+            {
+                PrintError("Your configuration file contains an error. Using default configuration values.");
+                LoadDefaultConfig();
+            }
+        }
+
+        protected override void SaveConfig() => Config.WriteObject(_config);
+
+        protected override void LoadDefaultConfig() => _config = new Configuration();
+
+        #endregion
+
+        #region Data
+
+        private class Data
+        {
+
+        }
+
+        private void LoadData()
+        {
+            if (Interface.Oxide.DataFileSystem.ExistsDatafile($"{Name}/data"))
+                data = Interface.Oxide.DataFileSystem.ReadObject<Dictionary<ulong, Data>>(
+                    $"{Name}/data");
+            else data = new Dictionary<ulong, Data>();
+            Interface.Oxide.DataFileSystem.WriteObject($"{Name}/data", data);
+        }
+
+        private void OnServerSave()
+        {
+            SaveData();
+        }
+
+        private void SaveData()
+        {
+            if (data != null) Interface.Oxide.DataFileSystem.WriteObject($"{Name}/data", data);
+        }
+
+        #endregion
+
+        #region OxideHooks
+
+        private void OnServerInitialized()
+        {
+            if (!ImageLibrary)
+            {
+                if (ImageLibraryCheck == 3)
+                {
+                    PrintError("ImageLibrary not found!Unloading");
+                    Interface.Oxide.UnloadPlugin(Name);
+                    return;
+                }
+
+                timer.In(1, () =>
+                {
+                    ImageLibraryCheck++;
+                    OnServerInitialized();
+                });
+                return;
+            }
+
+            LoadData();
+        }
+
+        private void Unload()
+        {
+            SaveData();
+        }
+
+        private void OnPlayerConnected(BasePlayer player)
+        {
+            if (player == null || data.ContainsKey(player.userID)) return;
+            data.Add(player.userID, new Data());
+        }
+
+        #endregion
+
+        #region Commands
+
+
+        #endregion
+
+        #region Functions
+
+
+
+        #endregion
+
+        #region UI
+
+
+        #endregion
+    }
+}
